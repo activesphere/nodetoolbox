2f7e8b8bc032c588f44eda52d6de77088468a61f
V Sreekanth <sreeix@gmail.com>
2012-07-17 23:40:55 +0530

    Adding npm synch an minor tweaks

--- script/npm_sync.js
+++ script/npm_sync.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-var gs = require('../lib/github_sync');
+var ns = require('../lib/npm_sync');
 
 var start = new Date();
 console.log("Starting the sync at "+ new Date());
-gs.github(function(err, res){
-  console.log("Github Sync completed successfully");
+ns.npm(function(err, res){
+  console.log("Npm Sync completed successfully");
   console.log("Ending the sync at "+ new Date());
   console.log("It took "+ (new Date() - start) + " millies");
   process.exit(0);
