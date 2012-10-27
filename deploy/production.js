74edd41c01556d77155e738d1be04dec7eca6c7e
V Sreekanth <sreeix@gmail.com>
2012-10-27 13:20:41 +0530

    Correcting the post/pre deploy function names

--- deploy/production.js
+++ deploy/production.js
@@ -3,11 +3,11 @@ module.exports = production =   {
   hosts : [{host: "toolbox-new", location:"~/apps/nodetoolbox"}],
   repository: { type: "git", url: "git://github.com/sreeix/nodetoolbox2.git", branch: "master"},
   deploymentType: "npm",
-  pre_deploy: function setupfolders (done) {
+  predeploy: function setupfolders (done) {
     console.log("pre deploy");
     done();
   },
-  post_deploy: function cleanup (done) {
+  postdeploy: function cleanup (done) {
     return this.cleanup(done);
   }
 };
