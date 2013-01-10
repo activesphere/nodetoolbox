0c5101870842129c75ddc32add1a2f10b131a94a
V Sreekanth <sreeix@gmail.com>
2013-01-10 23:18:47 +0530

    Updating the githubsync to add author info also

--- script/github_sync.js
+++ script/github_sync.js
@@ -4,8 +4,14 @@ var gs = require('../lib/github_sync');
 var start = new Date();
 console.log("Starting the sync at "+ new Date());
 gs.github(function(err, res){
-  console.log("Github Sync completed successfully");
-  console.log("Ending the sync at "+ new Date());
-  console.log("It took "+ (new Date() - start) + " millies");
-  process.exit(0);
+	if(err){
+	  console.log("Synch failed to complete");
+	  console.log(err);
+	  process.exit(1);
+	}else {
+	  console.log("Github Sync completed successfully");
+	  console.log("Ending the sync at "+ new Date());
+	  console.log("It took "+ (new Date() - start) + " millies");
+	  process.exit(0);
+	}
 });
