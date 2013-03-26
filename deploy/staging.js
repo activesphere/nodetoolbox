560b0884f83c4ad08ca9437c46f745c5066a1e95
V Sreekanth <sreeix@gmail.com>
2013-03-26 17:41:06 +0530

    Using loggers

--- deploy/staging.js
+++ deploy/staging.js
@@ -1,14 +1,14 @@
 module.exports = staging =   {
   name: 'toolbox',
-  hosts : [{host: "192.168.2.38", location:"~/apps/nodetoolbox"}],
+  hosts : [{host: "192.168.1.134", location:"~/apps/nodetoolbox"}],
   repository: { type: "git", url: "git://github.com/sreeix/nodetoolbox2.git", branch: "master"},
   deploymentType: "npm",
   predeploy: function setupfolders (done) {
-    console.log("pre deploy");
+    this.logger.info("pre deploy");
     done();
   },
   postdeploy: function cleanup (done) {
-    console.log("Post deploy")
+    this.logger.info("Post deploy")
     this.cleanup(done);
   }
 };
